7aa75fc746a579e91feb3ec0e9a8c1e42cf3b0a0
Marcin Tojek <tojek.m@gmail.com>
2017-06-10 16:31:58 +0200

    Support reading data from stdin

--- main.go
+++ main.go
@@ -1,26 +1,28 @@
 package main
 
 import (
-	"log"
 	"os"
+	"flag"
+	"log"
 )
 
 func main() {
-	if len(os.Args) != 3 {
-		log.Fatalf("Usage: %s data_file pattterns_file", os.Args[0])
-	}
+	var pArg = flag.String("p", "patterns", "patterns file")
+	flag.Parse()
 
-	dataFile, err := os.Open(os.Args[1])
-	if err != nil {
-		log.Fatalf("Error occurred while opening data_file: %v", err)
-	}
-	defer dataFile.Close()
-
-	patternsFile, err := os.Open(os.Args[2])
+	patternsFile, err := os.Open(*pArg)
 	if err != nil {
 		log.Fatalf("Error occurred while opening patterns_file: %v", err)
 	}
 	defer patternsFile.Close()
+
+	var dataFile = os.Stdin
+	if len(os.Args) == 4 {
+		dataFile, err = os.Open(flag.Arg(0))
+		if err != nil {
+			log.Fatalf("Error occurred while opening data_file: %v", err)
+		}
+	}
 
 	status := OrderedIntersect(dataFile, patternsFile, os.Stdout)
 	os.Exit(status)
